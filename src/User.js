32fbe156d3d24f971b7ef006627397bd520047b8
LoisClinton <loisclinton@hotmail.co.uk>
2023-10-24 19:52:51 +0100

    completed User.js

--- src/User.js
+++ src/User.js
@@ -1,5 +1,25 @@
 class User {
   // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+
+  login(password) {
+    //check if password is the same as this instance of password
+    if (password === this.password) {
+      this.loggedIn = true;
+    } else {
+      // if it isnt the same throw error
+      throw new Error("incorrect password");
+    }
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
 
-module.exports = User
+module.exports = User;
